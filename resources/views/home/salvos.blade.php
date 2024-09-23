9c3a6a15052e5eeaea2a4f059bab4e08e3f499af
Rebecca <151586136+rebecca-silva@users.noreply.github.com>
2024-09-23 10:21:36 -0300

    Add files via upload

---
+++ resources/views/home/salvos.blade.php
@@ -0,0 +1,9 @@
+@extends('templates.template-home')
+
+@section('links-css')
+    <link rel="stylesheet" href="{{url('assets/css/salvos.css')}}">
+@endsection
+
+@section('cont-home')
+
+@endsection
