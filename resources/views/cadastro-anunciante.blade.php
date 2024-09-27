8e7db854a840e542846b734916f0078e12e54aa1
Rebecca <151586136+rebecca-silva@users.noreply.github.com>
2024-09-27 12:01:25 -0300

    Add files via upload

--- resources/views/cadastro-anunciante.blade.php
+++ resources/views/cadastro-anunciante.blade.php
@@ -7,7 +7,7 @@
     <title>Materna - Cadastro</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{url('assets/css/style-cadastro.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/cadastro-anunciante.css')}}">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
