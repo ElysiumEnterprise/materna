5c3e32d7f11fe5b56880b45c8cf69a18ef416c3e
toory <112172698+VitoriaBarroso@users.noreply.github.com>
2024-09-30 10:35:37 -0300

    teste

--- resources/views/home/feed.blade.php
+++ resources/views/home/feed.blade.php
@@ -16,10 +16,10 @@
 
        <!-- Seção do Select para Escolher o Conteúdo -->
        <div class="cont-select">
+       <select id="conteudo-select" onchange="atualizarImagem()">
             <label for="conteudo-select">Escolha um Conteúdo:</label>
-            <select id="conteudo-select">
-                <option value="">Selecione um conteúdo</option>
-                <option value="adolescencia">Adolescência</option>
+                <option>Selecione um conteúdo</option>
+                <option value="alimentacao">Alimentação</option>
                 <option value="gravidez">Gravidez</option>
                 <option value="maes_solo">Mães Solo</option>
                 <option value="receitas">Receitas</option>
@@ -121,9 +121,9 @@
 
                 <section class='card-assunto'>
                     <div class="teste">
-                    <i class="fa-solid fa-circle-info icon"></i>
+                    <img src="{{ url('assets/img/icons/Alimentação.png') }}" alt="" class="img-assunto">
                     <div class="texto-card">
-                        <a href="#" class="link-card"><h4 class="txt-card-assunto">Adolescência</h4></a>
+                        <a href="#" class="link-card"><h4 class="txt-card-assunto"></h4></a>
                     </div>
                     </div>
                     
@@ -132,9 +132,10 @@
 
                 <section class='card-assunto'>
                     <div class="teste">
-                    <i class="fa-solid fa-circle-info icon"></i>
+                    <img src="{{ url('assets/img/icons/Maternidade Solo.png') }}" alt="" class="img-assunto">
+                   
                     <div class="texto-card">
-                        <a href="#" class="link-card"><h4 class="txt-card-assunto">Gravidez</h4></a>
+                        <a href="#" class="link-card"><h4 class="txt-card-assunto"></h4></a>
                     </div>
                     </div>
                     
@@ -142,27 +143,30 @@
 
                 <section class='card-assunto'>
                 <div class="teste">
-                    <i class="fa-solid fa-circle-info icon"></i>
+                <img src="{{ url('assets/img/icons/Puerperio.png') }}" alt="" class="img-assunto">
+                   
                     <div class="texto-card">
-                        <a href="#" class="link-card"><h4 class="txt-card-assunto">Mães Solo</h4></a>
+                        <a href="#" class="link-card"><h4 class="txt-card-assunto"></h4></a>
                     </div>
                     </div>
                 </section>
 
                 <section class='card-assunto'>
                 <div class="teste">
-                    <i class="fa-solid fa-circle-info icon"></i>
+                <img src="{{ url('assets/img/icons/Gestação.png') }}" alt="" class="img-assunto">
+                   
                     <div class="texto-card">
-                        <a href="#" class="link-card"><h4 class="txt-card-assunto">Receitas</h4></a>
+                        <a href="#" class="link-card"><h4 class="txt-card-assunto"></h4></a>
                     </div>
                     </div>
                 </section>
 
                 <section class='card-assunto'>
                 <div class="teste">
-                    <i class="fa-solid fa-circle-info icon"></i>
+                <img src="{{ url('assets/img/icons/Amamentação.png') }}" alt="" class="img-assunto">
+                  
                     <div class="texto-card">
-                        <a href="#" class="link-card"><h4 class="txt-card-assunto">Lazer</h4></a>
+                        <a href="#" class="link-card"><h4 class="txt-card-assunto"></h4></a>
                     </div>
                     </div>
                 </section>
